01ead95389b824af60f1ae30d8765fc736800fe6
387646983 <387646983@qq.com>
2025-08-14 22:19:40 +0800

    Template

---
+++ 0.ELAB_NANO_Template/project/stm32f103zet6/usr/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @author ZC (387646983@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-14
+ * 
+ * 
+ */
+#include "bsp.h"
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  BSP_Init();
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
